0636039ddce77b7a15bc5559d2d69e673421ea70
Christian Ledgard <christianledgard@gmail.com>
2018-10-10 21:01:04 -0500

    Semi Final

--- PY01/main.cpp
+++ PY01/main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <ctime>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -142,11 +143,7 @@ string dia_a_fecha(int dias_a_sumar) {
     return resultado;
 }
 
-
-
-
-
-void calculo_tiempos(float pedido[3][3], float velocidad[3][3][4],string tipo_pedido[3]){
+void calculo_tiempos(float pedido[3][3], float velocidad[3][3][4],string tipo_pedido[3],float ANALISIS_Tiempo[3]){
 
     float tiempo_total = 0,temporal1, suma = 0;
     float tiempos_pedido[3];
@@ -175,7 +172,7 @@ void calculo_tiempos(float pedido[3][3], float velocidad[3][3][4],string tipo_pe
 
     // Calculo de la fecha de ENTREGA
 
-    int dia,mes, anno, hoy_dia, hoy_mes, hoy_anno, dias_mes;
+    int dia;
 
 
 
@@ -184,8 +181,8 @@ void calculo_tiempos(float pedido[3][3], float velocidad[3][3][4],string tipo_pe
         dia = int((tiempos_pedido[l]/24));
         if(tiempos_pedido[l]/24!=dia)
             dia++;
-        cout << "Recoger el pedido de " << tipo_pedido[l] << " el " << dia_a_fecha(dia) << ", en " << dia << " dias." <<  endl;
-
+        cout << "Si solo el producto " << tipo_pedido[l] << " cominza a fabricarse, podra recogerse el " << dia_a_fecha(dia) << ", en " << dia << " dias." <<  endl;
+        ANALISIS_Tiempo[l] = dia;
 
     }
 
@@ -194,14 +191,13 @@ void calculo_tiempos(float pedido[3][3], float velocidad[3][3][4],string tipo_pe
 }
 
 
-void calculo_materia_prima(float Kg_PRENDAS[3], float Kg_Hilos[3]){
+void calculo_materia_prima(float Kg_PRENDAS[3], float Kg_Hilos[3], float ANALISIS_MateriaPrima[3]){
     float hilo_jersey;
     float hilo_pique;
     float hilo_franela;
 
-
     
-    hilo_jersey= Kg_PRENDAS[0]/0.7;
+    hilo_jersey = Kg_PRENDAS[0]/0.7;
     Kg_Hilos[0]=hilo_jersey;
     hilo_pique= Kg_PRENDAS[1]/0.5;
     Kg_Hilos[1]=hilo_pique;
@@ -212,26 +208,38 @@ void calculo_materia_prima(float Kg_PRENDAS[3], float Kg_Hilos[3]){
     cout<<"Seran necesarios "<<hilo_pique<<" Kg de hilo de Pique."<<endl;
     cout<<"Seran necesarios "<<hilo_franela<<" Kg de hilo de Franela."<<endl;
 
+    ANALISIS_MateriaPrima[0]=hilo_jersey;
+    ANALISIS_MateriaPrima[1]=hilo_pique;
+    ANALISIS_MateriaPrima[2]=hilo_franela;
+
 }
 
     
-void calculo_costos(float pedido[3][3], float Kg_PRENDAS[3], float Kg_Hilos[3]){
+void calculo_costos(float pedido[3][3], float Kg_PRENDAS[3], float Kg_Hilos[3], float ANALISIS_Ganancias[3]){
     //Precios de Venta - Costos de Produccion
     float Costo_Hilos=10;
 
     //Orden de Precio de venta: Polos, camisas y cuellos
-    float Costos_Jersey[3]={30, 50, 5};
-    float Costos_Pique[3]={60, 75, 10};
-    float Costos_Franela [3]={60, 80, 15};
+    float Costos_Jersey[3] = {30, 50, 5};
+    float Costos_Pique[3] = {60, 75, 10};
+    float Costos_Franela[3] = {60, 80, 15};
 
 
     float Costo_Total=0, Ingreso_Total=0;
     float Ingreso_Jersey=0, Ingreso_Pique=0, Ingreso_Franela=0;
+    float Costo_Jersey, Costo_Pique, Costo_Franela;
 
 
     for (int k = 0; k < 3; ++k) {
         Costo_Total+=Kg_Hilos[k]*Costo_Hilos;
     }
+
+    Costo_Jersey=Kg_Hilos[1]*Costo_Hilos;
+    Costo_Pique=Kg_Hilos[2]*Costo_Hilos;
+    Costo_Franela=Kg_Hilos[3]*Costo_Hilos;
+
+
+
     for (int i = 0; i < 3 ; ++i) {
         for (int j = 0; j < 3 ; ++j) {
             if (i == 0) {
@@ -246,24 +254,122 @@ void calculo_costos(float pedido[3][3], float Kg_PRENDAS[3], float Kg_Hilos[3]){
         }
     }
 
+
+
     Ingreso_Total=(Ingreso_Franela+Ingreso_Jersey+Ingreso_Pique);
-    cout<<"El costo total por fabricar todas las prendas es: s/."<<Costo_Total<<endl;
+    cout<< "\n";
+    cout<< "--------------------------------------------------------";
+    cout<< "\n";
+    cout<<"El ingreso por la venta de las prendas de Jersey es: S/ "<<Ingreso_Jersey<<endl;
+    cout<<"El ingreso por la venta de las prendas de Pique es: S/ "<<Ingreso_Pique<<endl;
+    cout<<"El ingreso por la venta de las prendas de Franela es: S/ "<<Ingreso_Franela<<endl;
+    cout<< endl;
+    cout<< endl;
+    cout<<"El ingreso TOTAL es: S/ "<<Ingreso_Total<<endl;
+    cout<<"El costo TOTAL es: S/ "<<Costo_Total<<endl;
 
-    cout<<"El ingreso por la venta de las prendas de Jersey es: s/."<<Ingreso_Jersey<<endl;
-    cout<<"El ingreso por la venta de las prendas de Pique es: s/."<<Ingreso_Pique<<endl;
-    cout<<"El ingreso por la venta de las prendas de Franela es: s/."<<Ingreso_Franela<<endl;
-    cout<<"El ingreso por la venta de todas las prendas fabricadas es: s/."<<Ingreso_Total<<endl;
+    cout<< "\n";
+    cout<< "--------------------------------------------------------";
+    cout<< "\n";
 
-    float Ganancia;
-    Ganancia=Ingreso_Total-Costo_Total;
-    cout<<"La ganancia sera: s/."<<Ganancia<<endl;
+    cout<< endl;
+    cout<< "Ganancias por productos: "<< endl;
+    cout<< "La ganancia de las prendas de Jersey es: S/ "<<Ingreso_Jersey-Costo_Jersey<<endl;
+    cout<< "La ganancia de las prendas de Pique es: S/ "<<Ingreso_Pique-Costo_Pique<<endl;
+    cout<< "La ganancia de las prendas de Franela es: S/ "<<Ingreso_Franela-Costo_Franela<<endl;
 
+    ANALISIS_Ganancias[0] = Ingreso_Jersey-Costo_Jersey;
+    ANALISIS_Ganancias[1] = Ingreso_Pique-Costo_Pique;
+    ANALISIS_Ganancias[2] = Ingreso_Franela-Costo_Franela;
+
+    cout<<"Ganancia TOTAL: s/."<<Ingreso_Total-Costo_Total<<endl;
+
+    cout<< "\n";
+    cout<< "-------------------------------------------------------------------------"<< endl;
+    cout<< "-------------------------- FIN DEL INFORME ------------------------------"<< endl;
+    cout<< "-------------------------------------------------------------------------"<< endl;
+    cout<< "\n";
 
     //        Polos      Camisas      Cuellos
     //Jersey   0,0        0,1            0,2
     //Pique    1,0        1,1            1,2
     //Franela  2,0        2,1            2,2
 }
+
+void ANALISIS(string TipoPedido[3],float Tiempo[3], float MateriaPrima[3], float Ganancias[3]){
+
+    cout << "//////////////////////////////////////////////////////////////////////" << endl;
+    cout << "\n";
+    cout << "-------- Inicio del Analisis ----------" << endl;
+    cout << "\n";
+
+    cout << "La fabrica se va a demorar:" << endl;
+    cout << "\n";
+
+    cout <<  "Tipo Pedido" << setw(15)<< "Tiempo" << setw(20) << "Ganancia" << setw(25) << "Ganancia/Tiempo" << setw(25)<< "Materia Prima"<< endl;
+
+    for (int i = 0; i < 3; ++i) {
+
+        cout <<  TipoPedido[i] << setw(15)<< fixed << setprecision(0) << Tiempo[i]<< " dias"<< setw(15) << "S/"<< fixed << setprecision(2) << Ganancias[i] << setw(20) << fixed << setprecision(2) << Ganancias[i]/Tiempo[i] << setw(25) << fixed << setprecision(2) << MateriaPrima[i]<< "Kg"<< endl;
+
+    }
+
+    //CALCULO DE EFECTIVIDAD: GANANCIA ENTRE TIEMPO
+
+    cout << "\n";
+    cout << "\n";
+
+
+    float Eficiencia[3], OrdenRecomendado[3];
+    for (int j = 0; j < 3; ++j) {
+        Eficiencia[j] = Ganancias[j]/Tiempo[j];
+    }
+
+    float MAX = 0.0, MIN = INT8_MAX;
+
+    for(int k = 0; k < 3; ++k) {
+        if(Eficiencia[k]>MAX){
+            MAX=Eficiencia[k];
+            OrdenRecomendado[0]=k;
+        }
+    }
+
+    for(int k = 0; k < 3; ++k) {
+        if(Eficiencia[k]<MIN){
+            MIN=Eficiencia[k];
+            OrdenRecomendado[2]=k;
+        }
+    }
+
+    for (int l = 0; l < 3; ++l) {
+        if(Eficiencia[l]!=MAX and Eficiencia[l]!=MIN)
+            OrdenRecomendado[1]=l;
+    }
+
+    cout << "El software recomienda piorizar las prendas por este orden: "<< endl;
+
+
+    for (int m = 0; m < 3; ++m) {
+
+        cout << m+1 << ". " << TipoPedido[int(OrdenRecomendado[m])]<< endl;
+
+    }
+
+    cout << "\n";
+    cout << "-------- Cronograma de Produccion ----------" << endl;
+    cout << "\n";
+
+    int tiempo_a_cuenta=0;
+
+    for (int n = 0; n < 3; ++n) {
+
+    cout << n+1 << ". "<< TipoPedido[int(OrdenRecomendado[n])] << " --- del "<< dia_a_fecha(tiempo_a_cuenta) << " al " << dia_a_fecha((Tiempo[int(OrdenRecomendado[n])])+tiempo_a_cuenta) << endl;
+    tiempo_a_cuenta += Tiempo[int(OrdenRecomendado[n])];
+
+    }
+
+}
+
 
 int main() {
 
@@ -302,15 +408,17 @@ int main() {
     };
 
 
+    float ANALISIS_Tiempo[3];
+    float ANALISIS_MateriaPrima[3];
+    float ANALISIS_Ganancias[3];
+
     ingresar_pedidos(pedido, tipo_prenda, tipo_pedido, Kg_PRENDAS);
     visualizar_pedido(pedido, tipo_prenda, tipo_pedido);
+    calculo_tiempos(pedido,velocidad, tipo_pedido, ANALISIS_Tiempo);
+    calculo_materia_prima(Kg_PRENDAS, Kg_Hilos, ANALISIS_MateriaPrima);
+    calculo_costos(pedido,Kg_PRENDAS, Kg_Hilos, ANALISIS_Ganancias);
 
-    calculo_tiempos(pedido,velocidad, tipo_pedido);
-
-    cout<<"\n";
-    calculo_materia_prima(Kg_PRENDAS, Kg_Hilos);
-    cout<<"\n";
-    calculo_costos(pedido,Kg_PRENDAS, Kg_Hilos);
+    ANALISIS(tipo_pedido, ANALISIS_Tiempo,ANALISIS_MateriaPrima,ANALISIS_Ganancias);
 
     //IMPRIMIR VISUALIZACION DEL PEDIDO COMO MATRIZ
     // for (int i = 0; i < 3; ++i) {
