388bc4f3fc2497f60ff4cce379a80080e4104287
carlos-reategui <39816970+carlos-reategui@users.noreply.github.com>
2018-11-24 22:41:20 -0500

    Update CPrendas.h

--- Proyecto 2/CPrendas.h
+++ Proyecto 2/CPrendas.h
@@ -23,19 +23,23 @@ public:
 
     virtual float getKg_Prenda() const;
     virtual void setKg_Prenda(float Kg_Prenda);
+
+    virtual float calculo_tiempos(float pedido[3][3]) = 0;
 };
 
 class Polo : public Prendas
 {
 public:
     const float  Velocidad[3][4]{
-            {1,10,20,0.2},//Jersey
-            {1,10,20,0.22},//Pique
-            {1,10,20,0.25}}; //Franela
+            {1, 10, 20, 0.2},//Jersey
+            {1, 10, 20, 0.22},//Pique
+            {1, 10, 20, 0.25}}; //Franela
 public:
 
     Polo();
-    ~Polo();
+    virtual ~Polo();
+
+    float calculo_tiempos(float pedido[3][3]) override;
 };
 
 class Camisa : public Prendas
@@ -48,21 +52,25 @@ private:
     };
 public:
     Camisa();
-    ~Camisa();
+    virtual ~Camisa();
 
+    float calculo_tiempos(float pedido[3][3]) override;
 };
 
 class Cuellos:public Prendas
 {
 private:
     const float Velocidad[3][4]{
-            {1,10,20,0.5},//Jersey
-            {1,10,20,0.33},//Pique
-            {1,10,20,0.50},//Franela
+            {1, 10, 20, 0.5},//Jersey
+            {1, 10, 20, 0.33},//Pique
+            {1, 10, 20, 0.50},//Franela
     };
 public:
     Cuellos();
-    ~Cuellos();
+    virtual ~Cuellos();
+
+    float calculo_tiempos(float pedido[3][3]) override;
 };
+
 
 #endif //PY01_CPRENDAS_H
